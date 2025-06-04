88e864767f3d60a37628371038f903edbb541b67
Riquelmer Añez Joffre <118318455+Reemolke@users.noreply.github.com>
2025-06-04 09:16:26 +0000

    pixel-rmo

--- src/firebase.js
+++ src/firebase.js
@@ -8,9 +8,9 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA0XAMqX6jmGzry-2BPegAEkNiEynW9G_U",
-  authDomain: "pixelpal-30370.firebaseapp.com",
+  authDomain: "pixelpal-rmo.firebaseapp.com/",
   projectId: "pixelpal-30370",
-  storageBucket: "pixelpal-30370.firebasestorage.app",
+  storageBucket: "pixelpal-rmo.firebasestorage.app",
   messagingSenderId: "854639217260",
   appId: "1:854639217260:web:712fd312a04582425b6649",
   measurementId: "G-EMGKCPD7PN"
