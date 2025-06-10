27e25d0678a6a73ce54161d090363769f7094eb7
Reemolke <riquelmeraj@gmail.com>
2025-06-11 01:03:21 +0200

    botones con hover

--- src/components/title.js
+++ src/components/title.js
@@ -1,14 +1,12 @@
 function Title({ home }) {
   return (
     <div style={{display: "flex",justifyContent: "space-evenly",width: "100%"}}>
-      <div className="back">
-        <h1>Exit</h1>
+      <div className="back" onClick={home}>
       </div>
-      <div className="title" onClick={home}>
+      <div className="title">
         <h1>Pixelpal</h1>
       </div>
-      <div className="log out">
-        <h1>Exit</h1>
+      <div className="logout">
       </div>
     </div>
     
